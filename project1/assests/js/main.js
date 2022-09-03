8518e88ed63a33b27f6676880341f8596a8a1212
Sergey87 <serge.techno@gmail.com>
2022-09-03 19:08:24 +0300

    added some js

--- project1/assests/js/main.js
+++ project1/assests/js/main.js
@@ -1,33 +1,48 @@
-console.log('test');
+// console.log('test');
 
-const posts = [
-  {
-    title: 'Heading post',
-    content:
-      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat dolorem molestiae quibusdam reiciendis. Beatae quisquam aliquam doloribus, explicabo architecto dolores! Ducimus eligendi enim eos nulla placeat rerum fugiat voluptatibus aperiam.',
-  },
-  {
-    title: 'Heading post',
-    content:
-      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat dolorem molestiae quibusdam reiciendis. Beatae quisquam aliquam doloribus, explicabo architecto dolores! Ducimus eligendi enim eos nulla placeat rerum fugiat voluptatibus aperiam.',
-  },
-  {
-    title: 'Heading post',
-    content:
-      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat dolorem molestiae quibusdam reiciendis. Beatae quisquam aliquam doloribus, explicabo architecto dolores! Ducimus eligendi enim eos nulla placeat rerum fugiat voluptatibus aperiam.',
-  },
-];
+// const posts = [
+//   {
+//     title: 'Heading post',
+//     content:
+//       'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat dolorem molestiae quibusdam reiciendis. Beatae quisquam aliquam doloribus, explicabo architecto dolores! Ducimus eligendi enim eos nulla placeat rerum fugiat voluptatibus aperiam.',
+//   },
+//   {
+//     title: 'Heading post',
+//     content:
+//       'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat dolorem molestiae quibusdam reiciendis. Beatae quisquam aliquam doloribus, explicabo architecto dolores! Ducimus eligendi enim eos nulla placeat rerum fugiat voluptatibus aperiam.',
+//   },
+//   {
+//     title: 'Heading post',
+//     content:
+//       'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat dolorem molestiae quibusdam reiciendis. Beatae quisquam aliquam doloribus, explicabo architecto dolores! Ducimus eligendi enim eos nulla placeat rerum fugiat voluptatibus aperiam.',
+//   },
+// ];
 
-const getArticleElem = ({ title, content }) => `<article>
+// articleSection.style.backgroundColor = 'lightBlue';
+
+const getArticleElem = ({ title, body }) => `<article>
       <h2>${title}</h2>
       <div>
-        <p>${content}</p>
+        <p>${body}</p>
       </div>
     </article>`;
 
-const articleSection = document.querySelector('section');
-articleSection.style.backgroundColor = 'lightBlue';
+async function fetchPosts() {
+  try {
+    const response = await fetch(
+      'https://jsonplaceholder.typicode.com/posts?_limit=3'
+    );
 
-// articleSection.innerHTML = getArticleElem(posts[0]);
+    const posts = await response.json();
 
-articleSection.innerHTML = posts.map((post) => getArticleElem(post)).join('');
+    const articleSection = document.querySelector('section');
+
+    articleSection.innerHTML = posts
+      .map((post) => getArticleElem(post))
+      .join('');
+  } catch (error) {
+    alert('error fetch', error);
+  }
+}
+
+fetchPosts();
