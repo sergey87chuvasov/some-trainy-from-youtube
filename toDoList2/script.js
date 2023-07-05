5063f81345c7a87f45eb2768d3462dbc61902a8a
Sergey87 <serge.techno@gmail.com>
2023-07-06 00:04:01 +0300

    finished to do list project

--- toDoList2/script.js
+++ toDoList2/script.js
@@ -1,2 +1,24 @@
 let inputBx = document.querySelector('#inputBx');
 let list = document.querySelector('#list');
+
+inputBx.addEventListener('keyup', function (e) {
+  if (e.key === 'Enter') {
+    addItem(this.value);
+    this.value = '';
+  }
+});
+
+let addItem = (inputBx) => {
+  let listItem = document.createElement('li');
+  listItem.innerHTML = `${inputBx}<i></i>`;
+
+  listItem.addEventListener('click', function () {
+    this.classList.toggle('done');
+  });
+
+  listItem.querySelector('i').addEventListener('click', function () {
+    listItem.remove();
+  });
+
+  list.appendChild(listItem);
+};
