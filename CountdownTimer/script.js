a2f3a5d4cfbbb61ef5861dd840318fd00c8c6fac
Sergey87 <serge.techno@gmail.com>
2023-07-07 23:30:40 +0300

    added mini js project - countdown timer - 2 part - finished project

--- CountdownTimer/script.js
+++ CountdownTimer/script.js
@@ -9,3 +9,44 @@ timersBtns.forEach((timerBtn) => {
     timerBtn.classList.add('active');
   });
 });
+
+// countdown
+const showTimer = document.querySelector('.countdown-value');
+let counter;
+const resetBtn = document.querySelector('.submitbtn');
+
+function countDown(duration) {
+  let stTime = duration * 60;
+  let displaySecond;
+
+  counter = setInterval(function () {
+    const minutes = Math.floor(stTime / 60);
+    const seconds = stTime % 60;
+
+    if (seconds < 10) {
+      displaySecond = '0' + seconds.toString();
+    } else {
+      displaySecond = seconds.toString();
+    }
+
+    showTimer.innerHTML = minutes + ':' + displaySecond;
+    stTime--;
+
+    if (stTime <= 0) {
+      clearInterval(counter);
+      showTimer.innerHTML = 'Complete!';
+      showTimer.classList.add('active');
+    }
+    resetBtn.classList.add('active');
+  }, 1000);
+
+  showTimer.innerHTML = '00:00';
+  showTimer.classList.remove('active');
+}
+
+function reset() {
+  clearInterval(counter);
+  showTimer.innerHTML = '00:00';
+  resetBtn.classList.remove('active');
+  showTimer.classList.remove('active');
+}
