6007ed226df66b3b7afccd74ebbedb512afdc293
Sergey87 <serge.techno@gmail.com>
2023-07-30 19:01:42 +0300

    added react training - work with data from forms - part2

--- 1-REACT TRAINING/Working With User Input Project/src/components/SomeInput.js
+++ 1-REACT TRAINING/Working With User Input Project/src/components/SomeInput.js
@@ -1,52 +1,79 @@
-import { useState, useRef } from 'react';
+import { useState } from 'react';
 
-const SomeInput = (props) => {
-  const nameInputRef = useRef();
-
+const SomeInput = () => {
+  // const nameInputRef = useRef();
   const [enterName, setEnterName] = useState('');
-
   // valid value or not
-  const [isEnteredNameValid, setIsEnteredNameValid] = useState(true);
+  // const [isEnteredNameValid, setIsEnteredNameValid] = useState(false);
+  // touch to input
+  const [wasNameInputTouched, setWasNameInputTouched] = useState(false);
+
+  const isEnteredNameValid = enterName.trim() !== '';
+  const isNameInputInvalid = !isEnteredNameValid && wasNameInputTouched;
 
   const nameInputChangeHandler = (e) => {
     setEnterName(e.target.value);
+
+    // if (e.target.value.trim() !== '') {
+    //   setIsEnteredNameValid(true);
+    // }
+  };
+
+  //lost focus
+  const nameInputLostFocusHandler = (e) => {
+    setWasNameInputTouched(true);
+
+    // if (enterName.trim() === '') {
+    //   setIsEnteredNameValid(false);
+    // }
   };
 
   const formSubmitHandler = (e) => {
     e.preventDefault();
 
-    if (enterName.trim() === '') {
-      setIsEnteredNameValid(false);
+    setWasNameInputTouched(true);
+
+    // if (enterName.trim() === '') {
+    //   setIsEnteredNameValid(false);
+    //   return;
+    // }
+
+    if (!isEnteredNameValid) {
       return;
     }
 
     // if valid
-    setIsEnteredNameValid(true);
+    // setIsEnteredNameValid(true);
 
-    console.log(enterName); // 1 opt
-    console.log(nameInputRef.current.value); // obj.current.val - 2-opt
+    // console.log(enterName); // 1 opt
+    // console.log(nameInputRef.current.value); // obj.current.val - 2-opt
 
     setEnterName(''); // update to do empty value
     // nameInputRef.current.value = ''; - not best ptactice
+
+    setWasNameInputTouched(false);
   };
 
   // for add styles
-  const nameInputClasses = isEnteredNameValid
-    ? 'form-control'
-    : 'form-control invalid';
+  const nameInputClasses = isNameInputInvalid
+    ? 'form-control invalid'
+    : 'form-control';
 
   return (
     <form onSubmit={formSubmitHandler}>
       <div className={nameInputClasses}>
         <label htmlFor='name'>Введите Имя</label>
         <input
-          ref={nameInputRef}
+          // ref={nameInputRef}
           type='text'
           id='name'
           onChange={nameInputChangeHandler}
+          onBlur={nameInputLostFocusHandler}
           value={enterName}
         />
-        {!isEnteredNameValid && <p className='error-text'>Ведите имя</p>}
+        {isNameInputInvalid && (
+          <p className='error-text'>Нужно обязательно ввести имя</p>
+        )}
       </div>
       <div className='form-actions'>
         <button>Отправить</button>
