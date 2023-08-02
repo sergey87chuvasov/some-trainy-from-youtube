8eba2767f9273fdfaba536a607d3b3e2ca2f9cce
Sergey87 <serge.techno@gmail.com>
2023-08-02 21:00:54 +0300

    added react training - part 3

--- 1-REACT TRAINING/Working With User Input Project/src/components/SomeInput.js
+++ 1-REACT TRAINING/Working With User Input Project/src/components/SomeInput.js
@@ -8,9 +8,44 @@ const SomeInput = () => {
   // touch to input
   const [wasNameInputTouched, setWasNameInputTouched] = useState(false);
 
+  // const [isFormValid, setIsFormValid] = useState(false);
+
+  // validation name
+  let isFormValid = false;
   const isEnteredNameValid = enterName.trim() !== '';
   const isNameInputInvalid = !isEnteredNameValid && wasNameInputTouched;
 
+  // fo all form
+  // useEffect(() => {
+  //   if (isEnteredNameValid) {
+  //     setIsFormValid(true);
+  //   } else {
+  //     setIsFormValid(false);
+  //   }
+  // }, [isEnteredNameValid]);
+
+  //for email
+  const [enteredEmail, setEnteredEmail] = useState('');
+  const [wasEmailInputTouched, setWasEmailInputTouched] = useState(false);
+
+  const emailInputChangeHandler = (e) => {
+    setEnteredEmail(e.target.value);
+  };
+
+  const emailInputLostFocusHandler = (e) => {
+    setWasEmailInputTouched(true);
+  };
+
+  //valid for email
+  const isEnteredEmailValid = enteredEmail.includes('@');
+  const isEmailInputInvalid = !isEnteredEmailValid && wasEmailInputTouched;
+
+  // for all
+  if (isEnteredNameValid && isEnteredEmailValid) {
+    isFormValid = true;
+  }
+
+  ///
   const nameInputChangeHandler = (e) => {
     setEnterName(e.target.value);
 
@@ -52,10 +87,19 @@ const SomeInput = () => {
     // nameInputRef.current.value = ''; - not best ptactice
 
     setWasNameInputTouched(false);
+
+    //email
+    setEnteredEmail('');
+    setWasEmailInputTouched(false);
   };
 
   // for add styles
   const nameInputClasses = isNameInputInvalid
+    ? 'form-control invalid'
+    : 'form-control';
+
+  // for add styles for email
+  const emailInputClasses = isEmailInputInvalid
     ? 'form-control invalid'
     : 'form-control';
 
@@ -75,8 +119,24 @@ const SomeInput = () => {
           <p className='error-text'>Нужно обязательно ввести имя</p>
         )}
       </div>
+
+      {/* email */}
+      <div className={emailInputClasses}>
+        <label htmlFor='email'>Введите email</label>
+        <input
+          type='email'
+          id='email'
+          onChange={emailInputChangeHandler}
+          onBlur={emailInputLostFocusHandler}
+          value={enteredEmail}
+        />
+        {isEmailInputInvalid && (
+          <p className='error-text'>Нужно обязательно ввести email</p>
+        )}
+      </div>
+
       <div className='form-actions'>
-        <button>Отправить</button>
+        <button disabled={!isFormValid}>Отправить</button>
       </div>
     </form>
   );
