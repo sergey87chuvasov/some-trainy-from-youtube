610337ea5bf131fddd4f448d4148471df2725be1
Sergey87 <serge.techno@gmail.com>
2023-08-30 22:02:33 +0300

    finished mini js project - Tic Tac Toe game

--- TicTacToe/script.js
+++ TicTacToe/script.js
@@ -1,14 +1,14 @@
 const cells = document.querySelectorAll('.cell');
 const playerXScoreSpan = document.querySelector('#playerXScore');
-const playerYScoreSpan = document.querySelector('#playerYScore');
-const resetBtn = document.querySelector('#resetBtn');
+const playerOScoreSpan = document.querySelector('#playerOScore');
+const resetBtn = document.querySelector('.resetBtn');
 const toastDiv = document.querySelector('.toast');
 const draws = document.querySelector('.draws');
 
 const playerX = 'X';
-const playerY = 'O';
+const playerO = 'O';
 let playerXScore = 0;
-let playerYScore = 0;
+let playerOScore = 0;
 let currentLevel = 1;
 let flag = true;
 let currentPlayer = playerX;
@@ -23,3 +23,106 @@ const winCombos = [
   [0, 4, 8],
   [2, 4, 8],
 ];
+
+for (let i = 0; i < cells.length; i++) {
+  cells[i].addEventListener('click', cellClicked);
+}
+
+function cellClicked(e) {
+  if (flag) {
+    if (e.target.innerHTML === '') {
+      e.target.appendChild(addImg(currentPlayer));
+      checkWinner();
+      checkDraw();
+
+      if (currentPlayer === playerX) {
+        currentPlayer = playerO;
+      } else {
+        currentPlayer = playerX;
+      }
+    }
+  }
+}
+
+function addImg(type) {
+  const img = document.createElement('img');
+  img.src = `${type}.png`;
+  return img;
+}
+
+function checkWinner() {
+  for (let i = 0; i < winCombos.length; i++) {
+    const winCombo = winCombos[i];
+    const cell1 = cells[winCombo[0]];
+    const cell2 = cells[winCombo[1]];
+    const cell3 = cells[winCombo[2]];
+
+    if (
+      cell1.innerHTML !== '' &&
+      cell1.innerHTML === cell2.innerHTML &&
+      cell1.innerHTML === cell3.innerHTML
+    ) {
+      // alert('win');
+      toast(`Player ${currentPlayer} wins!`);
+      updateScore();
+      flag = false;
+      currentLevel++;
+      setTimeout(() => {
+        reset();
+        toast(`level ${currentLevel}`);
+      }, 2000);
+    }
+  }
+}
+
+function checkDraw() {
+  if ([...cells].every((cell) => cell.innerHTML !== '')) {
+    toast('its a draw');
+    currentLevel++;
+    setTimeout(() => {
+      reset();
+      toast(`level ${currentLevel}`);
+    }, 2000);
+  }
+}
+
+function toast(msg) {
+  toastDiv.classList.add('show');
+  toastDiv.textContent = msg;
+
+  setTimeout(() => {
+    toastDiv.classList.remove('show');
+  }, 1000);
+}
+
+function updateScore() {
+  if (currentPlayer === playerX) {
+    playerXScore++;
+    playerXScoreSpan.textContent = playerXScore;
+  } else {
+    playerOScore++;
+    playerOScoreSpan.textContent = playerOScore;
+  }
+}
+
+function reset() {
+  cells.forEach((cell) => {
+    cell.innerHTML = '';
+  });
+  flag = true;
+}
+
+resetBtn.addEventListener('click', () => {
+  flag = false;
+  reset();
+  currentLevel = 1;
+  playerOScore = 0;
+  playerXScore = 0;
+  playerOScoreSpan.textContent = playerOScore;
+  playerXScoreSpan.textContent = playerXScore;
+  toast('game reset');
+  setTimeout(() => {
+    toast(`level ${currentLevel}`);
+    flag = true;
+  }, 2000);
+});
