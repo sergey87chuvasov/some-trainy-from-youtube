87646e71050de51a8ea0086a13dac2a46a8f7118
Sergey87 <serge.techno@gmail.com>
2023-11-21 22:51:16 +0300

    finished mini react project - website

--- 1-REACT TRAINING/Website Using React JS/src/App.jsx
+++ 1-REACT TRAINING/Website Using React JS/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import Background from './components/Background/Background';
 import Navbar from './components/Navbar/Navbar';
 import Hero from './components/Hero/Hero';
@@ -12,6 +12,14 @@ const App = () => {
 
   const [heroCount, setHeroCount] = useState(0);
   const [playStatus, setPlayStatus] = useState(false);
+
+  useEffect(() => {
+    setInterval(() => {
+      setHeroCount((count) => {
+        return count === 2 ? 0 : count + 1;
+      });
+    }, 3000);
+  });
 
   return (
     <div>
--- 1-REACT TRAINING/Website Using React JS/src/components/Background/Background.jsx
+++ 1-REACT TRAINING/Website Using React JS/src/components/Background/Background.jsx
@@ -7,16 +7,16 @@ import image3 from '../../assets/image33.png';
 const Background = ({ playStatus, heroCount }) => {
   if (playStatus) {
     return (
-      <video className='background' autoPlay loop muted>
+      <video className='background fade-in' autoPlay loop muted>
         <source src={video1} type='video/mp4' />
       </video>
     );
   } else if (heroCount === 0) {
-    return <img src={image1} className='background' />;
+    return <img src={image1} className='background fade-in' />;
   } else if (heroCount === 1) {
-    return <img src={image2} className='background' />;
+    return <img src={image2} className='background fade-in' />;
   } else if (heroCount === 2) {
-    return <img src={image3} className='background' />;
+    return <img src={image3} className='background fade-in' />;
   }
 };
 
