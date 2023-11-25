72cd856be987622ece8a7d713332a4b5d7ce3173
Sergey87 <serge.techno@gmail.com>
2023-11-25 13:08:02 +0300

    added 3 part and finished mini js project - countdown timer

--- CountDownTimer2/script.js
+++ CountDownTimer2/script.js
@@ -14,4 +14,29 @@ let x = setInterval(function () {
   let now = new Date(endDate).getTime();
   let countDown = new Date().getTime();
   let distance = now - countDown;
+
+  // calc time
+  let d = Math.floor(distance / (1000 * 60 * 60 * 24));
+  let h = Math.floor((distance % (100 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  let m = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  let s = Math.floor((distance % (1000 * 60)) / 1000);
+
+  // output result
+  days.innerHTML = d + '<br><span>Days</span>';
+  hours.innerHTML = h + '<br><span>Hours</span>';
+  minutes.innerHTML = m + '<br><span>Minutes</span>';
+  seconds.innerHTML = s + '<br><span>Seconds</span>';
+
+  //amimate stroke
+  dd.style.strokeDashoffset = 440 - (440 * d) / 365;
+  hh.style.strokeDashoffset = 440 - (440 * h) / 365;
+  mm.style.strokeDashoffset = 440 - (440 * m) / 365;
+  ss.style.strokeDashoffset = 440 - (440 * s) / 365;
+
+  // if the count down over
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById('time').style.display = 'none';
+    document.querySelector('.newYear').style.display = 'block';
+  }
 });
