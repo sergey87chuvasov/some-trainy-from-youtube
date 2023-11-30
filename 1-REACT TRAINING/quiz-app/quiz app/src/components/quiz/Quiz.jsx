344667bb1b247c659ebe2955f2ab05d42388a9c8
Sergey87 <serge.techno@gmail.com>
2023-11-30 23:39:54 +0300

    added mini react project - quiz app - 2 part

--- 1-REACT TRAINING/quiz-app/quiz app/src/components/quiz/Quiz.jsx
+++ 1-REACT TRAINING/quiz-app/quiz app/src/components/quiz/Quiz.jsx
@@ -1,17 +1,72 @@
-import React from 'react';
+import React, { useState, useRef } from 'react';
 import './Quiz.css';
+import { data } from '../../Assets/data';
 
 function Quiz() {
+  let [index, setIndex] = useState(0);
+  let [question, setQuestion] = useState(data[index]);
+  let [lock, setLock] = useState(false);
+
+  let Option1 = useRef(null);
+  let Option2 = useRef(null);
+  let Option3 = useRef(null);
+  let Option4 = useRef(null);
+
+  let option_array = [Option1, Option2, Option3, Option4];
+
+  const checkAns = (e, ans) => {
+    if (lock === false) {
+      if (question.ans === ans) {
+        e.target.classList.add('correct');
+        setLock(true);
+      } else {
+        e.target.classList.add('wrong');
+        setLock(true);
+        option_array[question.ans - 1].current.classList.add('correct');
+      }
+    }
+  };
+
   return (
     <div className='container'>
       <h1>Quiz APP</h1>
       <hr />
-      <h2>1. What the most popular car brand in the world?</h2>
+      <h2>
+        {index + 1}. {question.question}
+      </h2>
       <ul>
-        <li>Ferrari</li>
-        <li>Lada</li>
-        <li>Kia</li>
-        <li>BMW</li>
+        <li
+          ref={Option1}
+          onClick={(e) => {
+            checkAns(e, 1);
+          }}
+        >
+          {question.option1}
+        </li>
+        <li
+          ref={Option2}
+          onClick={(e) => {
+            checkAns(e, 2);
+          }}
+        >
+          {question.option2}
+        </li>
+        <li
+          ref={Option3}
+          onClick={(e) => {
+            checkAns(e, 3);
+          }}
+        >
+          {question.option3}
+        </li>
+        <li
+          ref={Option4}
+          onClick={(e) => {
+            checkAns(e, 4);
+          }}
+        >
+          {question.option4}
+        </li>
       </ul>
       <button>Next</button>
       <div className='index'>1 of 5 questions</div>
