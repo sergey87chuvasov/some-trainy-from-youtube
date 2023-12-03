8f0e7a2855c3a252f2974c57a058614d1cc235f5
Sergey87 <serge.techno@gmail.com>
2023-12-04 00:08:07 +0300

    added mini js project - Animated Number Spinner - 2 part - finished

--- NumberSpinner/script.js
+++ NumberSpinner/script.js
@@ -9,7 +9,14 @@ function plus() {
     return false;
   }
 
+  if (x >= 7) {
+    meter.style.background = '#f00';
+    meter.style.filter =
+      'drop-shadow(0 0 2.5px #f00) drop-shadow(0 0 10px #f00)';
+  }
+
   output.innerHTML = ++x;
+  meter.style.height = x * 10 + '%';
 }
 
 function minus() {
@@ -17,5 +24,12 @@ function minus() {
     return false;
   }
 
+  if (x <= 7) {
+    meter.style.background = '#0f0';
+    meter.style.filter =
+      'drop-shadow(0 0 2.5px #0f0) drop-shadow(0 0 10px #0f0)';
+  }
+
   output.innerHTML = --x;
+  meter.style.height = x * 10 + '%';
 }
