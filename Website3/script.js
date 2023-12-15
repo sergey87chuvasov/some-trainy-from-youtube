24b23275ef6b82a10e7fe825fa63b2e057ccb903
Sergey87 <serge.techno@gmail.com>
2023-12-15 23:29:11 +0300

    added mini js project - website 3 - 2 part

---
+++ Website3/script.js
@@ -0,0 +1,9 @@
+let navbar = document.querySelector('nav');
+
+window.onscroll = function () {
+  if (window.scrollY > 0) {
+    navbar.style.background = '#eefff9';
+  } else {
+    navbar.style.background = 'transparent';
+  }
+};
