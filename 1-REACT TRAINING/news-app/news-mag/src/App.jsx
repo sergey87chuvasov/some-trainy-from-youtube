d9e0d168b1ddb762e6f03f73d6b87179356d8677
Sergey87 <serge.techno@gmail.com>
2023-12-20 00:01:07 +0300

    finished mini react project - news app

--- 1-REACT TRAINING/news-app/news-mag/src/App.jsx
+++ 1-REACT TRAINING/news-app/news-mag/src/App.jsx
@@ -1,12 +1,14 @@
+import { useState } from 'react';
 import './App.css';
 import Navbar from './Components/Navbar';
 import NewsBoard from './Components/NewsBoard';
 
 function App() {
+  const [category, setCategory] = useState('general');
   return (
     <div>
-      <Navbar />
-      <NewsBoard />
+      <Navbar setCategory={setCategory} />
+      <NewsBoard category={category} />
     </div>
   );
 }
