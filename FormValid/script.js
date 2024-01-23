797be21bac5d9780dd13d39353b95fbf9f09a4cb
Sergey87 <serge.techno@gmail.com>
2024-01-23 22:22:30 +0300

    finished mini js project - validate form

--- FormValid/script.js
+++ FormValid/script.js
@@ -42,3 +42,45 @@ function validatePhone() {
   phoneError.innerHTML = '<i class="fa-solid fa-check"></i>';
   return true;
 }
+
+function validateEmail() {
+  var email = document.getElementById('contact-email').value;
+
+  if (email.length == 0) {
+    emailError.innerHTML = 'Email is required';
+    return false;
+  }
+
+  if (!email.match(/^[A-Za-z\._\-[0-9]*[@][A-Za-z]*[\.][a-z]{2,4}$/)) {
+    emailError.innerHTML = 'Email is Invalid';
+    return false;
+  }
+
+  emailError.innerHTML = '<i class="fa-solid fa-check"></i>';
+  return true;
+}
+
+function validateMessage() {
+  var message = document.getElementById('contact-message').value;
+  var required = 30;
+  var left = required - message.length;
+
+  if (left > 0) {
+    messageError.innerHTML = left + ' more characters required';
+    return false;
+  }
+
+  messageError.innerHTML = '<i class="fa-solid fa-check"></i>';
+  return true;
+}
+
+function validateForm() {
+  if (!validateName() || !validatePhone || !validateForm || !validateMessage) {
+    submitError.style.display = 'block';
+    submitError.innerHTML = `Please fix error to submit!`;
+    setTimeout(function () {
+      submitError.style.display = 'none';
+    }, 3000);
+    return false;
+  }
+}
