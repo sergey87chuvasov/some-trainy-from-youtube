2c523fce4bf31e4278697ebf85d8a55afd085090
Sergey87 <serge.techno@gmail.com>
2024-01-30 00:02:01 +0300

    added mini js project - 9 Dots Menu Drawer - 1 part

---
+++ 9-Dots/script.js
@@ -0,0 +1,6 @@
+let navigation = document.querySelector('.navigation');
+let menuToggle = document.querySelector('.menuToggle');
+
+menuToggle.onclick = function () {
+  navigation.classList.toggle('active');
+};
