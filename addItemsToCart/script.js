a138a669b474645bb031a3ca2a0a8bc6a5bce260
Sergey87 <serge.techno@gmail.com>
2024-02-05 00:58:06 +0300

    finished mini js project - Add To Cart Shopping

--- addItemsToCart/script.js
+++ addItemsToCart/script.js
@@ -70,3 +70,54 @@ function initApp() {
 }
 
 initApp();
+
+function addToCard(key) {
+  if (listCards[key] == null) {
+    listCards[key] = products[key];
+    listCards[key].quantity = 1;
+  }
+
+  reloadCard();
+}
+
+function reloadCard() {
+  listCard.innerHTML = '';
+  let count = 0;
+  let totalPrice = 0;
+
+  listCards.forEach((value, key) => {
+    totalPrice = totalPrice + value.price;
+    count = count + value.quantity;
+
+    if (value != null) {
+      let newDiv = document.createElement('li');
+      newDiv.innerHTML = `
+        <div><img src="./images/${value.image}" alt="item pic" /></div>
+        <div>${value.name}</div>
+        <div>${value.price.toLocaleString()}</div>
+        <div>${value.quantity}</div>
+        <div>
+          <button onclick="changeQuantity(${key}, ${value.quantity - 1})">-
+          </button>
+            <div class="count">${value.quantity}</div>
+          <button onclick="changeQuantity(${key}, ${value.quantity + 1})">+
+          </button>
+        </div>
+        `;
+      listCard.appendChild(newDiv);
+    }
+  });
+
+  total.innerText = totalPrice.toLocaleString();
+  quantity.innerText = count;
+}
+
+function changeQuantity(key, quantity) {
+  if (quantity == 0) {
+    delete listCards[key];
+  } else {
+    listCards[key].quantity = quantity;
+    listCards[key].price = quantity * products[key].price;
+  }
+  reloadCard();
+}
