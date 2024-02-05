d3111ef717f16842be167844c1cea5e938e3a329
Sergey87 <serge.techno@gmail.com>
2024-02-05 23:50:51 +0300

    added mini react project - accordion - 1 part

---
+++ 1-REACT TRAINING/accordion/src/components/accordian/data.js
@@ -0,0 +1,23 @@
+const data = [
+  {
+    id: '1',
+    question: 'What is your name?',
+    answer: 'My name is John.',
+  },
+  {
+    id: '2',
+    question: 'How old are you?',
+    answer: 'I am 25 years old.',
+  },
+  {
+    id: '3',
+    question: 'Where are you from?',
+    answer: 'I am from New York.',
+  },
+  {
+    id: '4',
+    question: 'What is your favorite color?',
+    answer: 'My favorite color is blue.',
+  },
+];
+export default data;
